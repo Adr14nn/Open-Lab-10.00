0366a6727a212fb1bbd1cda67062a110f4b20adb
Adr144n <adiko1805@gmail.com>
2021-11-28 12:31:03 +0100

    10.02

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -7,13 +7,54 @@ namespace Open_Lab_10._00
         static void Main(string[] args)
         {
             Book LOTR = new Book();
-            LOTR.title = "LOTR";
-            Console.WriteLine(LOTR.title);
+            LOTR.Title("Život Pepika");
+            LOTR.Pages(20);
+            LOTR.Category("sci-fi");
+            LOTR.Author("Štefan");
+            LOTR.ReleaseDate(2022);
+            LOTR.vypisovač();
+                
+            
         }
     }
     class Book
     {
-        public string title;
+        private string title;
         private int pages;
+        private string category;
+        private string author;
+        private int releaseDate;
+        public void Title(string gettitle)
+        {
+            title = gettitle;
+        }
+        public void Pages(int getpages)
+        {
+            pages = getpages;
+
+        }
+        public void Category(string getcategory)
+        {
+            category = getcategory;
+        }
+        public void Author(string getauthor)
+        {
+            author = getauthor;
+        }
+        public void ReleaseDate(int getreleaseDate)
+        {
+            releaseDate = getreleaseDate;
+        }
+
+        public void vypisovač()
+        {
+            Console.WriteLine(title);
+            Console.WriteLine(pages);
+            Console.WriteLine(category);
+            Console.WriteLine(author);
+            Console.WriteLine(releaseDate);
+        }
+
+            
     }
 }
